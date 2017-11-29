9d2141878cd79a65ab25d18ec19774efd7ef958d
alexhoma <alexcm.14@gmail.com>
2017-11-29 11:53:14 +0100

    test(NavigationComponent/Pagination): tested small function to render the navigation of the PaginationComponent

--- src/Widgets/Pagination/__tests__/NavigationComponent.test.js
+++ src/Widgets/Pagination/__tests__/NavigationComponent.test.js
@@ -3,15 +3,58 @@
  */
 import {h} from 'preact';
 import {deep} from 'preact-render-spy';
-import PaginationComponent from './../PaginationComponent';
+import NavigationComponent from './../NavigationComponent';
 import Template from '../../Template';
 
 describe('<NavigationComponent />',() => {
-    it('should render', () => {
+    const defaultProps = {
+        classNames:`navigation-component-class`,
+        template: `(((Template)))`,
+    };
 
+    it('should render if isVisible is set to true', () => {
+        let tree = getNavigationComponent({
+            ...defaultProps,
+            isVisible: true
+        });
+
+        expect(tree).toMatchSnapshot();
+    });
+    it('should NOT render if isVisible is set to false', () => {
+        let tree = getNavigationComponent({
+            ...defaultProps,
+            isVisible: false
+        });
+
+        expect(tree).toMatchSnapshot();
+    });
+    it('should handleClick()', () => {
+        let mock = jest.fn();
+        let tree = getNavigationComponent({
+            ...defaultProps,
+            isVisible: true,
+            handleClick: mock
+        });
+
+        tree.find('.navigation-component-class').at(0).simulate('click');
+
+        expect(mock).toBeCalled();
     });
 
-    const getPaginationComponent = () => {
 
+    const getNavigationComponent = ({
+        isVisible,
+        classNames,
+        template,
+        handleClick
+    }) => {
+        return deep(
+            <NavigationComponent
+                isVisible={isVisible}
+                classNames={classNames}
+                template={template}
+                handleClick={handleClick}
+            />
+        );
     }
 });
